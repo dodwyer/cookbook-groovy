126cbbe13157a0344c71fec7c7b04c47d2e18b5c
David O'Dwyer <david@livewyer.com>
2014-08-20 12:16:44 +0100

    Standard variable format

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
-default[:groovy][:version]    = '2.2.1'
-default[:groovy][:home]       = '/usr/local/groovy'
-default[:groovy][:url]        = 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.1.zip'
-default[:groovy][:checksum]   = '11262c2a0883018d4c68d353479a84bf32a694706437a31b23e17e3f2db695dc'
+default['groovy']['version']    = '2.2.1'
+default['groovy']['home']       = '/usr/local/groovy'
+default['groovy']['url']        = 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.1.zip'
+default['groovy']['checksum']   = '11262c2a0883018d4c68d353479a84bf32a694706437a31b23e17e3f2db695dc'
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,10 +11,10 @@ include_recipe "java"
 include_recipe "ark"
 
 ark "groovy" do
-  url node[:groovy][:url]
-  checksum node[:groovy][:checksum]
-  home_dir node[:groovy][:home]
-  version node[:groovy][:version]
+  url node['groovy']['url']
+  checksum node['groovy']['checksum']
+  home_dir node['groovy']['home']
+  version node['groovy']['version']
   append_env_path true
   action :install
 end
@@ -22,5 +22,5 @@ end
 template "/etc/profile.d/groovy_home.sh" do
   mode 0755
   source "groovy_home.sh.erb"
-  variables(:groovy_home => node[:groovy][:home])
+  variables(:groovy_home => node['groovy']['home'])
 end
